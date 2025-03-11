72b3b2d9a6d79d71a8bd5e63bf530f33e0ac05e7
Eli Kiprop <elikiprop42@gmail.com>
2025-03-11 08:57:40 -0700

    MediConnect project system

---
+++ mediapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
+    path('detail/', views.detail, name='detail'),
+    path('price/', views.price, name='price'),
+    path('search/', views.search, name='search'),
+    path('service/', views.service, name='service'),
+    path('team/', views.team, name='team'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+]
---
+++ mediapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def appointment(request):
+    return render(request, 'appointment.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def detail(request):
+    return render(request, 'detail.html')
+
+def price(request):
+    return render(request, 'price.html')
+
+def search(request):
+    return render(request, 'search.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
